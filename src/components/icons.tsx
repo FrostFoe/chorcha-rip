6594589db6a22d141b22ba1f5047b528fb333eae
FrostFoe <frostfoe@gmail.com>
2025-08-10 14:47:29 +0000

    The nav item প্রচ্ছদ will be প্রশ্ন ব্যাংক and fir the question bank pag

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -28,8 +28,8 @@ export const FacebookIcon = (props: SVGProps<SVGSVGElement>) => (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
+    fill="#1877F2"
+    stroke="#1877F2"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
@@ -41,17 +41,117 @@ export const FacebookIcon = (props: SVGProps<SVGSVGElement>) => (
 export const GoogleIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     {...props}
-    xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2z" />
-    <path d="M12 2a10 10 0 0 0-7.07 17.07c.32-.24 3.51-2.59 3.51-2.59a4 4 0 0 1 7.12 0s3.19 2.35 3.51 2.59A10 10 0 0 0 12 2z" />
+    <path
+      d="M22.56 12.25C22.56 11.45 22.49 10.68 22.36 9.92H12V14.45H18.42C18.13 15.99 17.22 17.32 15.82 18.23V21.09H20.03C22.04 19.21 22.56 16.05 22.56 12.25Z"
+      fill="#4285F4"
+    />
+    <path
+      d="M12 23C14.97 23 17.45 22.04 19.28 20.42L15.82 18.23C14.81 18.89 13.51 19.26 12 19.26C9.13 19.26 6.69 17.38 5.75 14.8H1.38V17.68C3.23 20.93 7.27 23 12 23Z"
+      fill="#34A853"
+    />
+    <path
+      d="M5.75 14.8C5.54 14.22 5.42 13.61 5.42 13C5.42 12.39 5.54 11.78 5.75 11.2L1.38 8.32C0.5 10.05 0 11.47 0 13C0 14.53 0.5 15.95 1.38 17.68L5.75 14.8Z"
+      fill="#FBBC05"
+    />
+    <path
+      d="M12 6.74C13.66 6.74 15.09 7.35 16.03 8.23L19.35 4.91C17.45 3.1 14.97 2 12 2C7.27 2 3.23 4.07 1.38 7.32L5.75 10.2C6.69 7.62 9.13 5.74 12 5.74Z"
+      fill="#EA4335"
+    />
   </svg>
+);
+
+
+// Institution Icons
+export const SatCollegeIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/ff6347/ff6347.png" {...props} width="32" height="32" />;
+export const BupIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/ffeb3b/ffeb3b.png" {...props} width="32" height="32" />;
+export const MaritimeIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/bdbdbd/bdbdbd.png" {...props} width="32" height="32" />;
+export const BuetIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/f44336/f44336.png" {...props} width="32" height="32" />;
+export const GucchoIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/2196f3/2196f3.png" {...props} width="32" height="32" />;
+export const RuetIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/ffc107/ffc107.png" {...props} width="32" height="32" />;
+export const KuetIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/4caf50/4caf50.png" {...props} width="32" height="32" />;
+export const CuetIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/00bcd4/00bcd4.png" {...props} width="32" height="32" />;
+export const IutIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/e91e63/e91e63.png" {...props} width="32" height="32" />;
+export const ButexIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/f06292/f06292.png" {...props} width="32" height="32" />;
+export const MistIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/9c27b0/9c27b0.png" {...props} width="32" height="32" />;
+export const MedicalIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/8bc34a/8bc34a.png" {...props} width="32" height="32" />;
+export const DentalIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/ff9800/ff9800.png" {...props} width="32" height="32" />;
+export const AfmcIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/cddc39/cddc39.png" {...props} width="32" height="32" />;
+export const IbaIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/3f51b5/3f51b5.png" {...props} width="32" height="32" />;
+export const NursingIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/ffb300/ffb300.png" {...props} width="32" height="32" />;
+export const DuIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/03a9f4/03a9f4.png" {...props} width="32" height="32" />;
+export const RuIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/26a69a/26a69a.png" {...props} width="32" height="32" />;
+export const JuIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/43a047/43a047.png" {...props} width="32" height="32" />;
+export const CuIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/00acc1/00acc1.png" {...props} width="32" height="32" />;
+export const JnuIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/c0ca33/c0ca33.png" {...props} width="32" height="32" />;
+export const AgriIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/fbc02d/fbc02d.png" {...props} width="32" height="32" />;
+export const BcsIcon = (props: SVGProps<SVGSVGElement>) => <image href="https://placehold.co/32x32/f4511e/f4511e.png" {...props} width="32" height="32" />;
+
+
+// Subject Icons
+export const LaptopIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M20 16V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v9m16 0H4m16 0 1.28 2.56a1 1 0 0 1-.9 1.44H3.62a1 1 0 0 1-.9-1.44L4 16" />
+  </svg>
+);
+export const Physics1Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M4 11a8 8 0 0 1 16 0M4 11v1M12 12v1m8-1v1m-7-6a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm-4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm8 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"/>
+        <path d="M6 16v2a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-2H6Z"/>
+    </svg>
+);
+export const Physics2Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M12 12V6l-2 2m4 0-2-2m0 6V4m6 8h-2m-8 0H4m12 5H8a2 2 0 0 0-2 2v1h12v-1a2 2 0 0 0-2-2Z"/>
+        <path d="M14 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+    </svg>
+);
+export const Chemistry1Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M9 3h6m-3 0v4m-4 2 2-2 2 2m-2 2v10m-4-6h8m-8 3h8"/>
+        <path d="M9 21h6"/>
+    </svg>
+);
+export const Chemistry2Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="m5.5 18.5 1-1m12-12-1 1m-4 4-1-1m-1 5-1-1m-3 7 1-1m1-11 1-1m-5 3-1 1m2 2-1 1m9-4 1-1"/>
+        <path d="M3 10v4a2 2 0 0 0 2 2h.5a2.5 2.5 0 0 1 2.5 2.5V21h4v-2.5a2.5 2.5 0 0 1 2.5-2.5H19a2 2 0 0 0 2-2v-4H3Z"/>
+    </svg>
+);
+export const HigherMath1Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M3 21 21 3m-9 18h9v-9"/>
+    </svg>
+);
+export const HigherMath2Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M16 19h4m-2-2v4m-4-6h-4v-4m-2 4h-2m10-6V7a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h6"/>
+    </svg>
+);
+export const Biology1Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2Z"/>
+        <path d="M12 12s4-3 4-6m-8 6s-4-3-4-6m4 6s-3 4-6 4m6-4s3 4 6 4"/>
+    </svg>
+);
+export const Biology2Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M21.5 12a9.5 9.5 0 1 1-19 0 9.5 9.5 0 0 1 19 0Z"/>
+        <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+    </svg>
+);
+export const Stats1Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M21.21 15.89A10 10 0 1 1 8.11 3.79M22 12A10 10 0 0 0 12 2v10h10Z"/>
+    </svg>
+);
+export const Stats2Icon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M3 21h18M7 21V12M12 21V3M17 21V8"/>
+    </svg>
 );
