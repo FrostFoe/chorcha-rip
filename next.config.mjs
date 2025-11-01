57e7689c5f5569267b7c409da7916d7ce43d7582
FrostFoe <frostfoe@gmail.com>
2025-11-01 15:06:00 +0000

    Ar kono issues ache deploy hobar por hote pare?

--- next.config.mjs
+++ next.config.mjs
@@ -1,55 +1,21 @@
-import createMDX from "@next/mdx"
-import rehypePrettyCode from "rehype-pretty-code"
-
-/** @type {import(\'next\').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.chorcha.net",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "assets.chorcha.net",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "play-lh.googleusercontent.com",
-        port: "",
-        pathname: "/**",
+        hostname: "picsum.photos",
       },
       {
         protocol: "https",
         hostname: "i.pravatar.cc",
-        port: "",
-        pathname: "/**",
       },
       {
         protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
+        hostname: "play-lh.googleusercontent.com",
       },
     ],
   },
 }
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [rehypePrettyCode],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
